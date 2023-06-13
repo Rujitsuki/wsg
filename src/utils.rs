7cf8613a284f6c97e563cb17292d829f7095c5c6
Niklas Ruzycki <2064210+Rujitsuki@users.noreply.github.com>
2023-06-14 01:12:03 +0200

    Add format_bytes function to format file sizes for a human being

--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,16 @@ pub fn dir_size(path: impl Into<PathBuf>) -> std::io::Result<u64> {
         Ok(acc + size)
     })
 }
+
+pub fn format_bytes(bytes: u64) -> String {
+    let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let mut value = bytes as f64;
+    let mut unit_index = 0;
+
+    while value >= 1000.0 && unit_index < units.len() - 1 {
+        value /= 1000.0;
+        unit_index += 1;
+    }
+
+    format!("{:.2} {}", value, units[unit_index])
+}
